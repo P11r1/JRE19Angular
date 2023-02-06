68df83560a8de00c7ebc2518bac75afa4e052ab6
Marko <Piirmarko92@gmail.com>
2023-02-06 21:40:56 +0200

    Angular examples and homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-practice';
+  title = 'SDA Front-End practice';
+  toShow = true;
+
+  fruits = ["apple", "orange", "banana", "mango"];
+
+  showAlertBox() {
+    alert("Click Me is clicked!");
+  }
+
+  doggies = ["robi", "rex", "bruno", "bosse"];
+
+  addDoggie () {
+    let dog = "Jimmy"
+    this.doggies.push(dog);
+  }
+
+
+  woofBox() {
+    alert("Woof woof!")
+  }
+
+
+
 }
